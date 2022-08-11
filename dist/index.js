7827f12ea5e2a71652b4a6aa8298e2095729f1aa
goman <vijonhero@gmail.com>
2022-08-11 17:36:50 +0800

    add more

--- dist/index.js
+++ dist/index.js
@@ -13,7 +13,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.utils = exports.AsyncArray = exports.queryLogsAndReceipts = exports.exportExcel = exports.FileTool = void 0;
+exports.FastEtherScan = exports.FastBscScan = exports.FastBTTCScan = exports.FastScan = exports.BscScan = exports.BlockScan = exports.utils = exports.AsyncArray = exports.queryLogsAndReceipts = exports.exportExcel = exports.FileTool = void 0;
 var fs_1 = require("./tools/fs");
 Object.defineProperty(exports, "FileTool", { enumerable: true, get: function () { return __importDefault(fs_1).default; } });
 var export_excel_1 = require("./tools/export-excel");
@@ -25,3 +25,11 @@ Object.defineProperty(exports, "AsyncArray", { enumerable: true, get: function (
 var ethers_extend_1 = require("./tools/ethers-extend");
 Object.defineProperty(exports, "utils", { enumerable: true, get: function () { return ethers_extend_1.utils; } });
 __exportStar(require("./tools/blockscan/index"), exports);
+var blockscan_1 = require("./tools/blockscan/blockscan");
+Object.defineProperty(exports, "BlockScan", { enumerable: true, get: function () { return blockscan_1.BlockScan; } });
+Object.defineProperty(exports, "BscScan", { enumerable: true, get: function () { return blockscan_1.BscScan; } });
+var fast_scan_1 = require("./tools/blockscan/fast-scan");
+Object.defineProperty(exports, "FastScan", { enumerable: true, get: function () { return fast_scan_1.FastScan; } });
+Object.defineProperty(exports, "FastBTTCScan", { enumerable: true, get: function () { return fast_scan_1.FastBTTCScan; } });
+Object.defineProperty(exports, "FastBscScan", { enumerable: true, get: function () { return fast_scan_1.FastBscScan; } });
+Object.defineProperty(exports, "FastEtherScan", { enumerable: true, get: function () { return fast_scan_1.FastEtherScan; } });
--- types/index.d.ts
+++ types/index.d.ts
@@ -4,3 +4,5 @@ export { queryLogsAndReceipts } from './tools/query-helper/logs';
 export { AsyncArray } from './tools/async-array';
 export { utils } from './tools/ethers-extend';
 export * from './tools/blockscan/index';
+export { BlockScan, BscScan } from './tools/blockscan/blockscan';
+export { FastScan, FastBTTCScan, FastBscScan, FastEtherScan } from './tools/blockscan/fast-scan';
